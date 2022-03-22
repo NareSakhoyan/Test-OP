88b46d436459604867e96d6315b5dd6f981121b0
Nare Sakhoyan <nare.sakhoyan@gmail.com>
2022-03-22 19:57:06 +0400

    added authorization

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require('jsonwebtoken');
 
+const {
+  getNotes,
+  getNoteByID,
+  createNote,
+  deleteNote,
+  login,
+  logout,
+} = require("./queries");
+const res = require("express/lib/response");
+const { user } = require("pg/lib/defaults");
 
-const { getNotes, getNoteByID, createNote, deleteNote } = require('./queries')
+const PORT = process.env.SERVER_PORT;
 
-const PORT = process.env.SERVER_PORT
-
-const app = express()
-
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  "/note",
+  (request, response, next) => {
+    const token = request.headers.authorization.split(' ')[1];
+    if (token) {
+      jwt.verify(token, "LOLIPOP", function (err, decoded) {
+        next();
+      });
+    } else response.status(403).send({ error: "You are not authorized" });
+  },
+  (req, res, next) => {
+    console.log("Request Type:", req.method);
+    next();
+  }
+);
 
-app.get("/notes/:id", getNoteByID)
-app.get("/notes", getNotes)
-app.post("/note", createNote)
-app.delete("/note/:id", deleteNote)
+app.get("/notes", getNotes);
+app.put("/login", login);
+app.put("/logout", logout);
+app.get("/note/:id", getNoteByID);
+app.post("/note", createNote);
+app.delete("/note/:id", deleteNote);
 app.get("/", (req, res) => {
-    res.status(200).send({"message": "If youdon't know how to use this API, then you should read the documentation"})
-})
-
+  res.status(200).send({
+    message:
+      "If youdon't know how to use this API, then you should read the documentation",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
-
+  console.log(`Server is running on port ${PORT}`);
+});
--- queries.js
+++ queries.js
@@ -1,13 +1,13 @@
-const res = require('express/lib/response')
+const res = require("express/lib/response");
+const jwt = require("jsonwebtoken");
 
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 
-const DB_USER = process.env.DB_USER 
-const DB_HOST = process.env.DB_HOST 
-const DB_DATABASE = process.env.DB_DATABASE 
-const DB_PASSWORD = process.env.DB_PASSWORD 
-const DB_PORT = process.env.DB_PORT
-
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
 
 const pool = new Pool({
   user: DB_USER,
@@ -15,62 +15,113 @@ const pool = new Pool({
   database: DB_DATABASE,
   password: DB_PASSWORD,
   port: DB_PORT,
-})
+});
 
 const getNotes = (request, response) => {
-    pool.query('SELECT * FROM notes ORDER BY id ASC', (error, results) => {
-        if (error) {
-            response.status(400).send({"error": error.message})
-            return;
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  pool.query("SELECT * FROM notes ORDER BY id ASC", (error, results) => {
+    if (error) {
+      response.status(400).send({ error: error.message });
+      return;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
 const getNoteByID = (request, response) => {
-    const id = parseInt(request.params.id)
+  const id = parseInt(request.params.id);
 
-    pool.query('SELECT title, isDone FROM notes WHERE id = $1', [id], (error, results) => {
-        if (error) {
-            response.status(400).send({"error": error.message})
-            return;
-        }
-        response.status(201).json(results.rows)
-    })
-}
+  pool.query(
+    "SELECT title, isDone FROM notes WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        response.status(400).send({ error: error.message });
+        return;
+      }
+      response.status(201).json(results.rows);
+    }
+  );
+};
 
 const createNote = (request, response) => {
-    const { title, idDone = false } = request.body
+  const { title, idDone = false } = request.body;
 
-    pool.query('INSERT INTO notes (title, isDone) VALUES ($1, $2) RETURNING *', [title, idDone], (error, results) => {
-        if (error) {
-            response.status(400).send({"error": error.message})
-            return;
-        }
-        response.status(201).send(results.rows[0])
-    })
-}
-
+  pool.query(
+    "INSERT INTO notes (title, isDone) VALUES ($1, $2) RETURNING *",
+    [title, idDone],
+    (error, results) => {
+      if (error) {
+        response.status(400).send({ error: error.message });
+        return;
+      }
+      response.status(201).send(results.rows[0]);
+    }
+  );
+};
 
 const deleteNote = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    pool.query('DELETE FROM notes WHERE id = $1 RETURNING *', [id], (error, results) => {
+  const id = parseInt(request.params.id);
+
+  pool.query(
+    "DELETE FROM notes WHERE id = $1 RETURNING *",
+    [id],
+    (error, results) => {
       if (error) {
-        response.status(400).send({"error": error.message})
+        response.status(400).send({ error: error.message });
         return;
       }
       if (results.rowCount == 0) {
-          response.status(200).send({"message": "Not a note with specified id"})
-          return;
+        response.status(200).send({ message: "Not a note with specified id" });
+        return;
       }
-      response.status(200).send(results.rows)
-    })
-  }
+      response.status(200).send(results.rows);
+    }
+  );
+};
+
+const login = (request, response) => {
+  const { username } = request.body;
+
+  //this could be a hash, but for now I'm letting a basic string
+  const session = Date.now() + "username";
+
+  pool.query(
+    "UPDATE users SET activesession = $1 RETURNING *",
+    [session],
+    (error, results) => {
+      if (error) {
+        response.status(400).send({ error: error.message });
+        return;
+      }
+      const token = jwt.sign(results.rows[0], "LOLIPOP");
+      response
+        .status(400)
+        .send({ message: "You are logged in, keep this token safe", token });
+    }
+  );
+};
+
+const logout = (request, response) => {
+  const { username } = request.body;
+
+  pool.query(
+    "UPDATE users SET activesession = $1 RETURNING *",
+    [null],
+    (error, results) => {
+      if (error) {
+        response.status(400).send({ error: error.message });
+        return;
+      }
+      response.status(201).send({ message: "User logged out successfully" });
+    }
+  );
+};
 
 module.exports = {
-    getNotes,
-    getNoteByID,
-    createNote,
-    deleteNote
+  getNotes,
+  getNoteByID,
+  createNote,
+  deleteNote,
+  login,
+  logout,
 };
